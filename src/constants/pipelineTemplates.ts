b7107e09ef00a4e32480d8035a85b9f23566dc0f
NucleusHQ <hi@nucleushq.io>
2024-11-25 20:16:45 +0530

    More data added

--- src/constants/pipelineTemplates.ts
+++ src/constants/pipelineTemplates.ts
@@ -12,18 +12,186 @@ export const templates = [
     },
     {
         id: '2',
-        title: 'Search a document Template',
-        description: 'Allows you to upload a file and ask questions in natural language about the file.',
+        title: 'Search a document',
+        description: 'Allows you to upload a file and ask questions.',
         icon: Antenna,
         categories: ['search', 'basic'],
         createdBy: 'alm3455'
     },
     {
         id: '3',
-        title: 'Search a knowledge base Template',
-        description: 'Ask a question about a centralized knowledge base that can contain documents, websites, videos, or live-synced integration.',
+        title: 'Search a knowledge base',
+        description: 'Ask a question about a centralized knowledge base.',
         icon: Antenna,
         categories: ['search', 'basic'],
         createdBy: 'alm3455'
     },
+    {
+        id: '4',
+        title: 'Search a website',
+        description: 'Allows you to ask questions about a website in natural language.',
+        icon: Antenna,
+        categories: ['search', 'basic'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '5',
+        title: 'Search a CSV',
+        description: 'This pipeline takes in a user query and an uploaded CSV.',
+        icon: Antenna,
+        categories: ['search', 'basic'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '6',
+        title: 'Search a document advanced',
+        description: 'Allows you to upload a file and ask questions',
+        icon: Antenna,
+        categories: ['search', 'basic'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '7',
+        title: 'Internal support assistant',
+        description: 'Answer internal employee.',
+        icon: Antenna,
+        categories: ['assistants', 'basic'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '8',
+        title: 'Employee onboarding assistant',
+        description: 'Answers employee onboarding questions.',
+        icon: Antenna,
+        categories: ['assistants', 'basic'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '9',
+        title: 'Customer support chatbot',
+        description: 'Uses LLMs and context from a knowledge base ',
+        icon: Antenna,
+        categories: ['chatbots', 'basic'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '10',
+        title: 'Lead collection chatbot',
+        description: 'Answer support questions and also collect prospect contact information.',
+        icon: Antenna,
+        categories: ['chatbots', 'basic'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '11',
+        title: 'Document task completion agent',
+        description: 'Provide three tasks for a LLM to complete',
+        icon: Antenna,
+        categories: ['assistants', 'basic'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '12',
+        title: 'AI with google search',
+        description: 'Allows you to ask a question that will be',
+        icon: Antenna,
+        categories: ['search', 'basic'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '13',
+        title: 'Investment memo writer',
+        description: 'Allows you to submit a company name and the pipeline .',
+        icon: Antenna,
+        categories: ['content-creation', 'finance'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '14',
+        title: 'Competitive research report writer',
+        description: 'Allows you to submit a company name and website.',
+        icon: Antenna,
+        categories: ['content-creation', 'finance'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '15',
+        title: 'Blog article generator',
+        description: 'This pipeline will generate a blog article.',
+        icon: Antenna,
+        categories: ['content-creation'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '16',
+        title: 'Search Gmail',
+        description: 'Search a semantic database of live-synced gmail emails.',
+        icon: Antenna,
+        categories: ['search', 'gmail'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '17',
+        title: 'Send Email Draft',
+        description: 'Send an email draft.',
+        icon: Antenna,
+        categories: ['gmail'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '18',
+        title: 'Search Google Drive',
+        description: 'Search a semantic database of live-synced google drive documents.',
+        icon: Antenna,
+        categories: ['search', 'google-drive'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '19',
+        title: 'Search Google Docs',
+        description: 'Search a semantic database.',
+        icon: Antenna,
+        categories: ['search', 'google-docs'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '20',
+        title: 'Append Text to Google Doc',
+        description: 'Append generated text.',
+        icon: Antenna,
+        categories: ['google-docs'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '21',
+        title: 'Search Slack',
+        description: 'Search a semantic database of live-synced slack messages.',
+        icon: Antenna,
+        categories: ['search', 'slack'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '22',
+        title: 'Send Slack Message',
+        description: 'Send a slack message.',
+        icon: Antenna,
+        categories: ['slack'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '23',
+        title: 'Send Discord Message',
+        description: 'Send a discord message.',
+        icon: Antenna,
+        categories: ['discord'],
+        createdBy: 'alm3455'
+    },
+    {
+        id: '24',
+        title: 'Search Discord',
+        description: 'Search a semantic database of live-synced discord messages.',
+        icon: Antenna,
+        categories: ['search', 'discord'],
+        createdBy: 'alm3455'
+    }
 ];
